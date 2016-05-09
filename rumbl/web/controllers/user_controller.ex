00fa5fc166564196d2c3beadfa310913f991b755
Vincent Theron <vincent@ntoggle.com>
2016-05-09 19:13:08 -0400

    ch5 - authentication

--- rumbl/web/controllers/user_controller.ex
+++ rumbl/web/controllers/user_controller.ex
@@ -3,13 +3,15 @@ defmodule Rumbl.UserController do
 
   alias Rumbl.User
 
+  plug :authenticate when action in [:index, :show]
+
   def index(conn, _params) do
-    users = Repo.all(Rumbl.User)
+    users = Repo.all(User)
     render conn, "index.html", users: users
   end
 
   def show(conn, %{"id" => id}) do
-    user = Repo.get(Rumbl.User, id)
+    user = Repo.get(User, id)
     render conn, "show.html", user: user
   end
 
@@ -19,11 +21,12 @@ defmodule Rumbl.UserController do
   end
 
   def create(conn, %{"user" => user_params}) do
-    changeset = User.changeset(%User{}, user_params)
+    changeset = User.registration_changeset(%User{}, user_params)
 
     case Repo.insert(changeset) do
       {:ok, user} ->
         conn
+          |> Rumbl.Auth.login(user)
           |> put_flash(:info, "#{user.name} created!")
           |> redirect(to: user_path(conn, :index))
 
@@ -31,5 +34,16 @@ defmodule Rumbl.UserController do
         render(conn, "new.html", changeset: changeset)
     end
   end
-  
+
+  defp authenticate(conn, _opts) do
+    if conn.assigns.current_user do
+      conn
+    else
+      conn
+      |> put_flash(:error, "You must be logged in.")
+      |> redirect(to: page_path(conn, :index))
+      |> halt()
+    end
+  end
+
 end
