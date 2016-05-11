45b7d28fe244af03ae5809d19d8b4d36a2b3fb88
Vincent Theron <vincent@ntoggle.com>
2016-05-11 17:26:18 -0400

    add resource video

--- rumbl/web/controllers/user_controller.ex
+++ rumbl/web/controllers/user_controller.ex
@@ -3,7 +3,7 @@ defmodule Rumbl.UserController do
 
   alias Rumbl.User
 
-  plug :authenticate when action in [:index, :show]
+  plug :authenticate_user when action in [:index, :show]
 
   def index(conn, _params) do
     users = Repo.all(User)
@@ -32,17 +32,6 @@ defmodule Rumbl.UserController do
 
       {:error, changeset} ->
         render(conn, "new.html", changeset: changeset)
-    end
-  end
-
-  defp authenticate(conn, _opts) do
-    if conn.assigns.current_user do
-      conn
-    else
-      conn
-      |> put_flash(:error, "You must be logged in.")
-      |> redirect(to: page_path(conn, :index))
-      |> halt()
     end
   end
 
